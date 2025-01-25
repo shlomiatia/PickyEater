02ea1ec0f02a0af905ff38dbc0b47d24654c2594
Anatnatz <122973316+Anatnatz@users.noreply.github.com>
2025-01-25 13:56:26 +0200

    intro and outro

--- Entities/Dialog/dialog.gd
+++ Entities/Dialog/dialog.gd
@@ -4,14 +4,14 @@ var animation_player: AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    animation_player = get_node("AnimationPlayer")
+	animation_player = get_node("AnimationPlayer")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-    pass
+	pass
 
 func ShowDialog(dialog: String, color: Color):
-    self.text = dialog
-    self.add_theme_color_override("font_color", color)
-    animation_player.seek(0)
-    animation_player.play("Show")
+	self.text = dialog
+	self.add_theme_color_override("font_color", color)
+	animation_player.seek(0)
+	animation_player.play("Show")
--- Scripts/StartBtn.gd
+++ Scripts/StartBtn.gd
@@ -1,14 +1,46 @@
+class_name MainScreen
 extends Button
 
-var new_scene = preload("res://Entities/Game/Game.tscn").instantiate()
 @onready var pow_food: PowFood = $"../../CanvasLayer2/PowFood"
+const INTRO: Array[String] = ["Danny! Time for Dinner!",
+"Coming mom!",
+"...",
+"Oh there you are Danny, if I'd waited any longer my hair would turn gray!",
+"...",
+"Your dinner is ready!",
+"...",
+"Oh no!",
+"Eat up, before it gets cold!",
+"Have you seen this thing bobby? It’s disgusting! I think mom is trying to poison me again.",
+"Wang Wang",
+"That’s right. I have to find a way to get rid of it without mom noticing, or I am doomed!",
+"..."
+]
+@onready var dialog_popup: DialogPopup = $"../../CanvasLayer2/DialogPopup"
+const INTRO_SPEAKERS: Array[String] = [dialog_popup.mom,
+dialog_popup.kid,
+dialog_popup.mom,
+dialog_popup.mom,
+dialog_popup.laugh,
+dialog_popup.mom,
+dialog_popup.food,
+dialog_popup.kid,
+dialog_popup.mom,
+dialog_popup.kid,
+dialog_popup.dog,
+dialog_popup.kid,
+dialog_popup.dog
+]
 
 func _on_pressed() -> void:
-	pow_food._pow_food(2)
 	#var current_scene = self.get_tree().current_scene
 	#self.get_tree().root.add_child(new_scene)
 	#self.get_tree().current_scene = new_scene
 	#current_scene.queue_free()
 	
-	#var dialog_popup = self.get_node("/root/Main/DialogPopup") as DialogPopup
-	#dialog_popup._start_dialog(dialog_popup.mom, ["hello", "hi? dont die"])
+	dialog_popup._start_dialog(INTRO_SPEAKERS, INTRO)
+
+func _broccoli() -> void:
+	pow_food._pow_food(0)
+	
+	
