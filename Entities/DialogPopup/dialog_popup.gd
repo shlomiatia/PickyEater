1668e3d6a9dd25d625da085ae5ececea9e16ce67
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-01-25 12:05:29 +0200

    aaa

--- Entities/DialogPopup/dialog_popup.gd
+++ Entities/DialogPopup/dialog_popup.gd
@@ -16,36 +16,36 @@ var current_speaker: String = kid
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+    pass
 
 
 func _start_dialog(speaker: String, dialog_content: Array[String]) -> void:
-	_hide_dialogs()
-	dialog = dialog_content
-	current_speaker = speaker
-	#color_rect.visible = true
-	button.visible = true
-	if(speaker==mom):
-		mom_dialog.visible = true
-		mom_label.text = dialog[0]
-	if(speaker==kid):
-		kid_dialog.visible = true
-		kid_label.text = dialog[0]
-	#show text 
+    _hide_dialogs()
+    dialog = dialog_content
+    current_speaker = speaker
+    #color_rect.visible = true
+    button.visible = true
+    if(speaker==mom):
+        mom_dialog.visible = true
+        mom_label.text = dialog[0]
+    if(speaker==kid):
+        kid_dialog.visible = true
+        kid_label.text = dialog[0]
+    #show text 
 
 func _on_button_pressed() -> void:
-	dialog = dialog.slice(1)
-	if(dialog.size() >= 1):
-		_start_dialog(mom if (current_speaker==kid) else kid, dialog)
-	else:
-		_hide_dialogs()
-		button.visible = false
+    dialog = dialog.slice(1)
+    if(dialog.size() >= 1):
+        _start_dialog(mom if (current_speaker==kid) else kid, dialog)
+    else:
+        _hide_dialogs()
+        button.visible = false
 
 func _hide_dialogs() -> void:
-	mom_dialog.visible = false
-	kid_dialog.visible = false
+    mom_dialog.visible = false
+    kid_dialog.visible = false
